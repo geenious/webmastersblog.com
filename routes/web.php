3e820a9ad86593a9057c82ae37f44a9b0248e055
Jeff Irwin <jeff.irwin@live.com>
2020-09-15 21:40:31 -0500

    init create/post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 Route::get('/create-post', function() {
     return view('admin/create-post');
 });
+
+Route::post('/post/create', [CreatePost::class]);
